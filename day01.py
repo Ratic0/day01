31bf4117854d04d604879d960a72e6ad6b63ed8f
KimTaeYeong <12170548@inha.edu>
2023-01-12 16:26:39 +0900

    day01_04gui

--- day01.py
+++ day01.py
@@ -1,6 +1,6 @@
-subjects={'열역학' : 'A+', '재료역학' : 'B+', '생산설계시스템' : 'A'}
-student = '김태영'
-subject = '생산설계시스템'
-print('%s 학생의 %s 과목의 성적은 %s 입니다' % (student, subject, subjects[subject])) # old style
-print("{0} 학생의 {1} 과목 성적은 {2} 입니다". format(student,subject,subjects[subject])) # mordern style
-print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]} 입니다') #f스트링 가장 최근
+import tkinter as tk
+
+win = tk.Tk()
+win.title('파이썬 1일차 Preview')
+win.geometry("300x400")
+win.mainloop()
